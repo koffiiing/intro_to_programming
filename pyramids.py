cc9e7635264861515d199656ddc65db817a67958
koffiiing <robfes89@gmail.com>
2026-09-15 12:04:48 -0400

    made changes and improvements to pyramids.py

--- pyramids.py
+++ pyramids.py
@@ -3,24 +3,45 @@ Robert Fesperman
 2026_09_13
 Great Pyramid Analysis
 """
+import math
 
-print("\n\n\t\t\t---- This program will show you some pretty interesting calculations related to the great pyramid! ----\n\n ")
+print("\n\n\t\t\t---- This program will show you some pretty interesting calculations related to the great pyramid! ----\n ")
 
 print("""
 
-Please select from the following: \n\nOption A: \n\n\tIt has been estimated that the Great Pyramid of Giza weighs around 5.9 million tons.
-It is also said that the pyramids only took 20 years to build.\n\tOption A will calculate exactly how much stone was moved per day to accomplish this.
-\n\nOption B,\n\nOption C\n
+Please select from the following:
+
+Option A:
+
+It has been estimated that the Great Pyramid of Giza weighs around 5.9 million tons.
+It is also said that the pyramids only took 20 years to build.
+Option A will calculate exactly how much stone would have had to have been moved each day to accomplish this seemingly impossible feet.
+
+Option B:
+
+Each side of the Great Pyramid is 230.4 meters and it's original height was 146.5m.
+Option B will calculate the perimeter of the Great Pyramid, divide the perimeter by its height, and display the result.
+Do you see a significance to this number?
+
+Option C:
+
+
 
 """)
 
-stone_per_day = 5_900_000
+# Variables used for calculations
+days_in_20y = 20 * 365
+stone_per_day = 5900000 / days_in_20y
 
 while True:
     user_input = input("Please enter A, B, C (or enter Q to quit): ").upper()
     if user_input == "A":
-        print(f"""\nThe ancient Egyptions would have had to move {stone_per_day} tons of stone per day to build the massive structure in only 20 years. """)
-    # do something
+        print(f"""
+
+The ancient Egyptions would have had to move roughly {math.trunc(stone_per_day)} tons
+or 17,000,000 pounds of stone per day to build the Great Pyramid of Giza in only 20 years.
+
+""")
 
     elif user_input == "B":
         pass
